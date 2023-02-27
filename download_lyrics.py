5ea1d7e1df7b0a534e5d530972910a268c1eb78b
Elena <34395235+ZotovaElena@users.noreply.github.com>
2023-02-27 10:56:58 +0100

    Update download_lyrics.py

--- download_lyrics.py
+++ download_lyrics.py
@@ -3,13 +3,16 @@
 Created on Wed Jun 19 14:14:48 2019
 @author: zotov
 """
-#we use the library for working with Genius
-
+import pandas as pd
+import json
+import os
 import lyricsgenius 
+
+from itertools import chain
 
 genius = lyricsgenius.Genius("BBMypsrFRNrEd68cKEkY7hLODx1wAOZ-1GfV8lOpKEdad4guY1Svo-1oB86T0fbE") #here goes your API key from Genius
 
-#make a list of groups and artists whose songs  you want to download, in this example this is all reggeaton singers
+#make a list of groups and artists whose songs  you want to download, in this example this is (almost) all reggeaton singers
 filename = 'REGGETON_artists.txt'
 with open(filename, encoding='utf-8') as f:
     reggeton_list = f.readlines()
@@ -32,11 +35,6 @@ for a in artist_list:
         song_list.append(song)
    
 #artist.save_lyrics()
-
-import pandas as pd
-import json
-import os
-
 #read jsons with lyrics
 songs_new = []
 for filename in os.listdir('./songs'):
@@ -51,8 +49,6 @@ df_songs = pd.DataFrame.from_dict(songs_new)
 
 list_songs_df = df_songs['songs'].values.tolist()
 
-from itertools import chain
-
 df_songs_col = pd.DataFrame(list(chain.from_iterable(list_songs_df)))
 
 frames = [df_songs['artist'], df_songs_col]
@@ -62,4 +58,4 @@ df = df.fillna('No info')
 df111 = df[~df.title.str.contains('duplicada')] #exclude duplicated lyrics
 df222= df111[~df111.title.str.contains('duplicado')]
 
-df222.to_csv("all_songs.csv", sep=';', encoding='utf-8', index=False) #save the dataframe with all songs
+df222.to_csv(filename.split('.')[0]+"_all_songs.tsv", sep='\t', encoding='utf-8', index=False) #save the dataframe with all songs
